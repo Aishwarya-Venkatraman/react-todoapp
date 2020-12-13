9cb716e2db4648b697e937852ebc89d0dd531508
Aishwarya Venkatraman <65395892+Aishwarya-Venkatraman@users.noreply.github.com>
2020-12-13 15:17:26 +0000

    todo app using state

---
+++ src/TodosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Clean the house",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Pay the bills",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Iron the clothes",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Catch up on coding",
+        completed: false
+    }
+]
+
+export default todosData
